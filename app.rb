86099a1900db8ff8bc9d6ed46db2958f41e5b2cb
Kelsey Fecho <kfecho30@gmail.com>
2018-04-07 13:58:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
 
+  get '/square/:num' do
+    @sqr = params[:num].to_i * params[:num].to_i
+    "#{@sqr}"
+  end
+
+  get '/say/:number/:phrase' do
+    binding.pry
+    @phrase = params[:phrase]
+    params[:number].times do
+      "#{@phrase}"
+    end
+  end
 end
